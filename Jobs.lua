19f8202ad43d41cf1d0701a22cdb767bdfa6bf29
Ramir-CODER <57854995+RamiR294@users.noreply.github.com>
2019-11-30 15:16:29 +0300

    Update Jobs.lua

--- Jobs.lua
+++ Jobs.lua
@@ -1,8 +1,16 @@
-Editdarkrp
-SDS
-S
-Hddh
-Sn
-Ggg
-DSAD
-DASDASDAS
+TEAM_CADET = DarkRP.createJob("Клон-Кадет", {
+    color = Color(0, 25, 255, 255),
+    model = {"models/player/kleiner.mdl"},
+    description = [[Готов служить ВАР]],
+    weapons = {},
+    command = "cadet",
+    max = 0,
+    salary = 15,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = false,
+    PlayerSpawn = function(ply) ply:SetHealth(100) GAMEMODE:SetPlayerSpeed(ply, GAMEMODE.Config.walkspeed * 0.75, GAMEMODE.Config.runspeed * 1.5) end,
+    PlayerDeath = function(ply) GAMEMODE:SetPlayerSpeed(ply, GAMEMODE.Config.walkspeed, GAMEMODE.Config.runspeed) end,
+ })
+ 
